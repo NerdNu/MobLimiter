91860b593eaf0eefa6351e9b97e00bf26c8a32d6
John Driscoll <johnoliverdriscoll@gmail.com>
2012-07-20 13:43:28 -0700

    Use EventHandlers for population cap enforcement instead of a scheduled task.

--- src/nu/nerd/moblimiter/MobLimiter.java
+++ src/nu/nerd/moblimiter/MobLimiter.java
@@ -1,58 +1,107 @@
 package nu.nerd.moblimiter;
 
 import org.bukkit.Chunk;
-import org.bukkit.World;
+import org.bukkit.craftbukkit.CraftChunk;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 
-public class MobLimiter extends JavaPlugin {
+public class MobLimiter extends JavaPlugin implements Listener {
 
-	@Override
-	public void onEnable() {
-		this.getConfig().options().copyDefaults(true);
-		this.getConfig().addDefault("MobLimiter.Max.LivingEntity", 100);
-		final int limit = this.getConfig().getInt("MobLimiter.Max.LivingEntity");
-		saveConfig();
-		getLogger().info("MobLimiter max set to: " + limit);
-		// cleanup extra mobs once every 5 minutes
-		getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
-			public void run() {
-				getLogger().info("Running MobLimiter");
-				for (World world : getServer().getWorlds()) {
-					for (Chunk chunk : world.getLoadedChunks()) {
-						int Mobs = 0;
-						for (Entity entity : chunk.getEntities()) {
-							// will get all mobs except players
-							if ((entity instanceof LivingEntity) && !(entity instanceof HumanEntity) && !(entity instanceof Monster)) {
-								Mobs++;
-								if (Mobs > limit) {
-									((LivingEntity) entity).remove();
-									//getLogger().info("Removed " + entity.getType());
-								}
-							}
-						}
-						for (Entity entity : chunk.getEntities()) {
-							// run through hostile mobs after, the passive mobs should clear first, lets remove them before passive, keep the llama to a minimum.
-							if (entity instanceof Monster) {
-								Mobs++;
-								if (Mobs > limit) {
-									((LivingEntity) entity).remove();
-									//getLogger().info("Removed " + entity.getType());
-								}
-							}
-						}
-					}
-				}
-				getLogger().info("Done Running MobLimiter");
-			}
-		}, 1200, 6000);
-	}
+    private static int limit;
 
-	@Override
-	public void onDisable() {
-		getServer().getScheduler().cancelTasks(this);
-	}
+    @Override
+    public void onEnable() {
+        this.getConfig().options().copyDefaults(true);
+        this.getConfig().addDefault("MobLimiter.Max.LivingEntity", 100);
+        limit = this.getConfig().getInt("MobLimiter.Max.LivingEntity");
+        saveConfig();
+        getLogger().info("MobLimiter max set to: " + limit);
+        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info(getDescription().getName() + " " + getDescription().getVersion() + " enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("MobLimiter max set to: " + limit);
+        getServer().getScheduler().cancelTasks(this);
+        getLogger().info(getDescription().getName() + " " + getDescription().getVersion() + " disabled.");
+    }
+
+    @EventHandler
+    public void onChunkLoad(final ChunkLoadEvent e) {
+        // Enforce population cap on chunk load
+        int Mobs = 0;
+        net.minecraft.server.Chunk chunk = ((CraftChunk) e.getChunk()).getHandle();
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < chunk.entitySlices[i].size(); j++) {
+                final Entity entity = ((net.minecraft.server.Entity) chunk.entitySlices[i].get(j)).getBukkitEntity();
+                // will get all passive mobs
+                if (isLimitedEntity(entity) && !(entity instanceof Monster)) {
+                    Mobs++;
+                    if (Mobs > limit) {
+                        ((LivingEntity) entity).remove();
+                        //getLogger().info("Removed " + entity.getType());
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < chunk.entitySlices[i].size(); j++) {
+                final Entity entity = ((net.minecraft.server.Entity) chunk.entitySlices[i].get(j)).getBukkitEntity();
+                // run through hostile mobs after, the passive mobs should clear first, lets remove them before passive, keep the llama to a minimum.
+                if (entity instanceof Monster) {
+                    Mobs++;
+                    if (Mobs > limit) {
+                        ((LivingEntity) entity).remove();
+                        //getLogger().info("Removed " + entity.getType());
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onCreatureSpawn(final CreatureSpawnEvent e) {
+        // Allow or deny creature spawn events based on the population of its chunk
+        if (e.isCancelled())
+            return;
+        int count = 0;
+        final Entity entity = e.getEntity();
+        // Count limitable entities in chunk directly
+        net.minecraft.server.Chunk chunk = ((CraftChunk) entity.getLocation().getChunk()).getHandle();
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < chunk.entitySlices[i].size(); j++) {
+                if (isLimitedEntity(chunk.entitySlices[i].get(j))) {
+                    count++;
+                }
+            }
+        }
+        if (count >= limit) {
+            // Prevent spawning
+            e.setCancelled(true);
+            return;
+        }
+    }
+
+    public boolean isLimitedEntity(final Object obj) {
+        Entity entity = null;
+        if (obj instanceof net.minecraft.server.Entity) {
+            entity = ((net.minecraft.server.Entity) obj).getBukkitEntity();
+        } else {
+            entity = (Entity) obj;
+        }
+        if (entity != null && entity instanceof LivingEntity &&
+            !(entity instanceof HumanEntity)) {
+            return true;
+        }
+        return false;
+    }
+
 }
