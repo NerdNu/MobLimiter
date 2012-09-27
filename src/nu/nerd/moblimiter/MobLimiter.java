046bf22916ece014cd18a0cf6e4702ef7ced6252
smcgregor <smcgregor@Obnivious.(none)>
2012-09-27 11:31:17 +1000

    passive removal, needs async?

--- src/nu/nerd/moblimiter/MobLimiter.java
+++ src/nu/nerd/moblimiter/MobLimiter.java
@@ -1,107 +1,77 @@
 package nu.nerd.moblimiter;
 
-import org.bukkit.Chunk;
-import org.bukkit.craftbukkit.CraftChunk;
+import java.util.HashMap;
+import java.util.Map;
+import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Monster;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkUnloadEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class MobLimiter extends JavaPlugin implements Listener {
 
-    private static int limit;
+    private Map<String, Integer> limits;
 
     @Override
     public void onEnable() {
         this.getConfig().options().copyDefaults(true);
-        this.getConfig().addDefault("MobLimiter.Max.LivingEntity", 100);
-        limit = this.getConfig().getInt("MobLimiter.Max.LivingEntity");
         saveConfig();
-        getLogger().info("MobLimiter max set to: " + limit);
         getServer().getPluginManager().registerEvents(this, this);
         getLogger().info(getDescription().getName() + " " + getDescription().getVersion() + " enabled.");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("MobLimiter max set to: " + limit);
         getServer().getScheduler().cancelTasks(this);
         getLogger().info(getDescription().getName() + " " + getDescription().getVersion() + " disabled.");
     }
+    
+    public Map<String, Integer> loadConfig() {
+//       All Mobs we care about
+//       Blaze, CaveSpider, Creeper, Enderman, Giant, PigZombie, Silverfish, Skeleton, Spider, Zombie
+//       Chicken, Cow, MushroomCow, Ocelot, Pig, Sheep, Wolf
+
+         Map<String, Integer> limconf = new HashMap<String, Integer>();
+//       Animals
+         limconf.put("Chicken", getConfig().getInt("limit.Chicken", 10));
+         limconf.put("Cow", getConfig().getInt("limit.Cow", 10));
+         limconf.put("MushroomCow", getConfig().getInt("limit.MushroomCow", 10));
+         limconf.put("Ocelot", getConfig().getInt("limit.Ocelot", 10));
+         limconf.put("Pig", getConfig().getInt("limit.Pig", 10));
+         limconf.put("Sheep", getConfig().getInt("limit.Sheep", 10));
+         limconf.put("Wolf", getConfig().getInt("limit.Wolf", 10));
+//       Monsters
+         limconf.put("Blaze", getConfig().getInt("limit.Blaze", 10));
+         limconf.put("CaveSpider", getConfig().getInt("limit.CaveSpider", 10));
+         limconf.put("Creeper", getConfig().getInt("limit.Creeper", 10));
+         limconf.put("Enderman", getConfig().getInt("limit.Enderman", 10));
+         limconf.put("Giant", getConfig().getInt("limit.Giant", 10));
+         limconf.put("PigZombie", getConfig().getInt("limit.PigZombie", 10));
+         limconf.put("Silverfish", getConfig().getInt("limit.Silverfish", 10));
+         limconf.put("Skeleton", getConfig().getInt("limit.Skeleton", 10));
+         limconf.put("Spider", getConfig().getInt("limit.Spider", 10));
+         limconf.put("Zombie", getConfig().getInt("limit.Zombie", 10));
+         return limconf;
+    }
 
     @EventHandler
-    public void onChunkLoad(final ChunkLoadEvent e) {
-        // Enforce population cap on chunk load
-        int Mobs = 0;
-        net.minecraft.server.Chunk chunk = ((CraftChunk) e.getChunk()).getHandle();
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < chunk.entitySlices[i].size(); j++) {
-                final Entity entity = ((net.minecraft.server.Entity) chunk.entitySlices[i].get(j)).getBukkitEntity();
-                // will get all passive mobs
-                if (isLimitedEntity(entity) && !(entity instanceof Monster)) {
-                    Mobs++;
-                    if (Mobs > limit) {
-                        ((LivingEntity) entity).remove();
-                        //getLogger().info("Removed " + entity.getType());
+    public void onChunkUnload(final ChunkUnloadEvent e) {
+        Map<String, Integer> count = new HashMap<String, Integer>();
+        for( Entity entity : e.getChunk().getEntities()) {
+            if((entity instanceof Animals) || (entity instanceof Monster)) {
+                if(count.get(entity.getType().name()) == null) {
+                    count.put(entity.getType().name(), 0);
+                }
+                int mbcount = count.get(entity.getType().name());
+                count.put(entity.getType().name(), ++mbcount);
+                if(count.get(entity.getType().name()) != null) {
+                    if(mbcount > count.get(entity.getType().name())) {
+                        entity.remove();
                     }
                 }
             }
         }
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < chunk.entitySlices[i].size(); j++) {
-                final Entity entity = ((net.minecraft.server.Entity) chunk.entitySlices[i].get(j)).getBukkitEntity();
-                // run through hostile mobs after, the passive mobs should clear first, lets remove them before passive, keep the llama to a minimum.
-                if (entity instanceof Monster) {
-                    Mobs++;
-                    if (Mobs > limit) {
-                        ((LivingEntity) entity).remove();
-                        //getLogger().info("Removed " + entity.getType());
-                    }
-                }
-            }
-        }
     }
-
-    @EventHandler
-    public void onCreatureSpawn(final CreatureSpawnEvent e) {
-        // Allow or deny creature spawn events based on the population of its chunk
-        if (e.isCancelled())
-            return;
-        int count = 0;
-        final Entity entity = e.getEntity();
-        // Count limitable entities in chunk directly
-        net.minecraft.server.Chunk chunk = ((CraftChunk) entity.getLocation().getChunk()).getHandle();
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < chunk.entitySlices[i].size(); j++) {
-                if (isLimitedEntity(chunk.entitySlices[i].get(j))) {
-                    count++;
-                }
-            }
-        }
-        if (count >= limit) {
-            // Prevent spawning
-            e.setCancelled(true);
-            return;
-        }
-    }
-
-    public boolean isLimitedEntity(final Object obj) {
-        Entity entity = null;
-        if (obj instanceof net.minecraft.server.Entity) {
-            entity = ((net.minecraft.server.Entity) obj).getBukkitEntity();
-        } else {
-            entity = (Entity) obj;
-        }
-        if (entity != null && entity instanceof LivingEntity &&
-            !(entity instanceof HumanEntity)) {
-            return true;
-        }
-        return false;
-    }
-
 }
