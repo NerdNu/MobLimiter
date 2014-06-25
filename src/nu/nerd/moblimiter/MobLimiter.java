a3e6b1687a5f3c035dbc4fb794d4eb1f806c0131
tompreuss <tompreuss@gmail.com>
2014-06-25 02:13:38 -0400

    Add informative /moblimiter command
    New /moblimiter command describes the purpose of MobLimiter to players in game and distributes information about the current configuration.

--- src/nu/nerd/moblimiter/MobLimiter.java
+++ src/nu/nerd/moblimiter/MobLimiter.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
@@ -311,4 +314,120 @@ public class MobLimiter extends JavaPlugin implements Listener {
             }
         }
     }
+    
+    /**
+     * Informative /moblimiter command to describe the purpose of MobLimiter to players 
+     * in game and distribute information about the current configuration.
+     */
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("moblimiter")) {
+            StringBuilder message = new StringBuilder();
+            message.append(ChatColor.GOLD);
+            message.append("This server runs a plugin called " + ChatColor.YELLOW + "MobLimiter"
+                + ChatColor.GOLD + " in order to manage the "
+                + "amount of mobs on the server, as too many mobs creates lag which affects "
+                + "everyone. Mobs are culled down to the following values per chunk on chunk "
+                + "unload, (-1 indicates no culling): ");
+            
+                // if sheep are all configured to the same amount, just say one value
+                // else put an asterisk and don't report the separate colors anyway,
+                // since there's so many
+            
+            if ((getConfig().getInt("sheepwhite") == getConfig().getInt("sheeporange")) &&
+                    (getConfig().getInt("sheeporange") == getConfig().getInt("sheepmagenta")) &&
+                    (getConfig().getInt("sheepmagenta") == getConfig().getInt("sheeplight_blue")) &&
+                    (getConfig().getInt("sheeplight_blue") == getConfig().getInt("sheepyellow")) &&
+                    (getConfig().getInt("sheepyellow") == getConfig().getInt("sheeplime")) &&
+                    (getConfig().getInt("sheeplime") == getConfig().getInt("sheeppink")) &&
+                    (getConfig().getInt("sheeppink") == getConfig().getInt("sheepgray")) &&
+                    (getConfig().getInt("sheepgray") == getConfig().getInt("sheepsilver")) &&
+                    (getConfig().getInt("sheepsilver") == getConfig().getInt("sheepcyan")) &&
+                    (getConfig().getInt("sheepcyan") == getConfig().getInt("sheeppurple")) &&
+                    (getConfig().getInt("sheeppurple") == getConfig().getInt("sheepblue")) &&
+                    (getConfig().getInt("sheepblue") == getConfig().getInt("sheepbrown")) &&
+                    (getConfig().getInt("sheepbrown") == getConfig().getInt("sheepgreen")) &&
+                    (getConfig().getInt("sheepgreen") == getConfig().getInt("sheepred")) &&
+                    (getConfig().getInt("sheepred") == getConfig().getInt("sheepblack")))
+                        {
+                            message.append(ChatColor.YELLOW + "sheep: " + ChatColor.GRAY
+                                + getConfig().getInt("sheepwhite"));
+                        }
+            else
+                        {
+                            message.append(ChatColor.YELLOW + "sheep*: " + ChatColor.GRAY
+                                + getConfig().getInt("sheepwhite"));                                
+                        }
+                
+            message.append(ChatColor.YELLOW + " chicken: " + ChatColor.GRAY 
+                    + getConfig().getInt("chicken"));
+            message.append(ChatColor.YELLOW + " cow: " + ChatColor.GRAY 
+                    + getConfig().getInt("cow"));
+            message.append(ChatColor.YELLOW + " mushroom_cow: " + ChatColor.GRAY 
+                    + getConfig().getInt("mushroom_cow"));
+            message.append(ChatColor.YELLOW + " ocelot: " + ChatColor.GRAY 
+                    + getConfig().getInt("ocelot"));
+            message.append(ChatColor.YELLOW + " pig: " + ChatColor.GRAY 
+                    + getConfig().getInt("pig"));
+            message.append(ChatColor.YELLOW + " wolf: " + ChatColor.GRAY 
+                    + getConfig().getInt("wolf"));
+            message.append(ChatColor.YELLOW + " blaze: " + ChatColor.GRAY 
+                    + getConfig().getInt("blaze"));
+            message.append(ChatColor.YELLOW + " cave_spider: " + ChatColor.GRAY 
+                    + getConfig().getInt("cave_spider"));
+            message.append(ChatColor.YELLOW + " creeper: " + ChatColor.GRAY 
+                    + getConfig().getInt("creeper"));
+            message.append(ChatColor.YELLOW + " enderman: " + ChatColor.GRAY 
+                    + getConfig().getInt("enderman"));
+            message.append(ChatColor.YELLOW + " ghast: " + ChatColor.GRAY 
+                    + getConfig().getInt("ghast"));
+            message.append(ChatColor.YELLOW + " silverfish: " + ChatColor.GRAY 
+                    + getConfig().getInt("silverfish"));
+            message.append(ChatColor.YELLOW + " skeleton: " + ChatColor.GRAY 
+                    + getConfig().getInt("skeleton"));
+            message.append(ChatColor.YELLOW + " spider: " + ChatColor.GRAY 
+                    + getConfig().getInt("spider"));
+            message.append(ChatColor.YELLOW + " squid: " + ChatColor.GRAY 
+                    + getConfig().getInt("squid"));
+            message.append(ChatColor.YELLOW + " villager: " + ChatColor.GRAY 
+                    + getConfig().getInt("villager"));
+            message.append(ChatColor.YELLOW + " zombie: " + ChatColor.GRAY 
+                    + getConfig().getInt("zombie"));
+            message.append(ChatColor.GOLD);
+            message.append(". To make up for the culling of passive farm animals, the plugin shortens "
+                    + "the growth rate of baby animals and the waiting time in between breeding "
+                    + "events, to approximately "
+                    + ChatColor.GRAY + getConfig().getInt("agecapbaby") / 20
+                    + ChatColor.GOLD + " and "
+                    + ChatColor.GRAY + getConfig().getInt("agecapbreed") / 20
+                    + ChatColor.GOLD + " seconds respectively. ");
+
+                    // explain spawn-limited, if set
+            
+                if (getConfig().isList("settings.spawn_limited")) {
+                    message.append("Note that mobs of type " + ChatColor.YELLOW
+                        + getConfig().getStringList("settings.spawn_limited") + ChatColor.GOLD
+                        + " are configured to be only new-spawn-limited and not culled, once their numbers "
+                        + "have reached the integer in the previous list no new mobs of that type will spawn "
+                        + "in that respective chunk. ");
+                }
+                
+                    // on nerd.nu we use KitchenSink to plump drops, which is because of
+                    // mob limiting, so mention that here 
+                    
+            message.append("Additionally, the drops of passive mobs "
+                    + "are plumped so that more items drop which allows for fewer animals to produce "
+                    + "more resources.  Tamed pet mobs, mobs custom named with a nametag, and mobs "
+                    + "wearing armor are not be culled.  If you have any questions please feel free "
+                    + "to contact a server administrator.");
+
+            sender.sendMessage(message.toString());
+        
+        }    
+            
+            return true;
+        
+    }
+    
 }
