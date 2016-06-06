85c046cc55ceb7bc59d3fa6fbc0db92f54506f24
redwallhp <mcredwallhp@gmail.com>
2016-06-06 04:24:39 -0400

    Implemented Chunkunloadlimiter

--- src/nu/nerd/moblimiter/limiters/ChunkUnloadLimiter.java
+++ src/nu/nerd/moblimiter/limiters/ChunkUnloadLimiter.java
@@ -1,7 +1,20 @@
 package nu.nerd.moblimiter.limiters;
 
+import nu.nerd.moblimiter.EntityHelper;
 import nu.nerd.moblimiter.MobLimiter;
+import nu.nerd.moblimiter.configuration.ConfiguredMob;
+import org.bukkit.Chunk;
+import org.bukkit.entity.Animals;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Monster;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.world.ChunkUnloadEvent;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -17,6 +30,51 @@ public class ChunkUnloadLimiter implements Listener {
     public ChunkUnloadLimiter() {
         plugin = MobLimiter.instance;
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onChunkUnload(final ChunkUnloadEvent event) {
+        removeMobs(event.getChunk());
+    }
+
+
+    /**
+     * Remove excess mobs in a chunk, if chunk unload culling is enabled for the mob type.
+     * This is the default behavior from MobLimiter 1.x.
+     * @param chunk The chunk to cull mobs in
+     */
+    public void removeMobs(Chunk chunk) {
+        Map<String, Integer> count = new HashMap<String, Integer>();
+        for (Entity entity : chunk.getEntities()) {
+
+            // Only Animals and Monsters are eligible for removal
+            if (entity.isDead() || !(entity instanceof Animals || entity instanceof Monster)) continue;
+
+            // Exempt special mobs
+            if (EntityHelper.isSpecialMob((LivingEntity) entity)) {
+                if (plugin.getConfiguration().debug()) {
+                    plugin.getLogger().info("Special mob exempted from removal: " + EntityHelper.getMobDescription(entity));
+                }
+                continue;
+            }
+
+            ConfiguredMob limits = plugin.getConfiguration().getLimits(entity.getType());
+            String key = entity.getType().toString();
+            int cap = limits.getCull();
+
+            Integer oldCount = count.get(key);
+            int mobCount = (oldCount == null) ? 1 : oldCount + 1;
+            count.put(key, mobCount);
+
+            if (cap > -1 && mobCount > cap) {
+                entity.remove();
+                if (plugin.getConfiguration().debug()) {
+                    plugin.getLogger().info("Chunk unload removed: " + EntityHelper.getMobDescription(entity));
+                }
+            }
+
+        }
     }
 
 
